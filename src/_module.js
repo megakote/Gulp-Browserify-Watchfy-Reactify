03049f979a5c1c72a348d050899b56acc2dca009
megakote <spam@mt2014.com>
2014-07-27 00:25:44 +0400

    Added console.info to test source map

--- src/_module.js
+++ src/_module.js
@@ -4,6 +4,10 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  componentDidMount: function() {
+    console.info('%cTest source map', 'background-color:#2C2C2C;color:#3A97DB');
+   },
+
   render: function() {
     return <div>Hello {this.props.name}</div>;
   }
